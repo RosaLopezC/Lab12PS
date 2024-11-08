53d2d4b8fb08413749c518e08609fcdad48fe937
aaronnole <aaron.nole@tecsup.edu.pe>
2024-11-07 22:38:42 -0500

    Método Actualizar

--- petclinic_integration_test/src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
+++ petclinic_integration_test/src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
@@ -9,15 +9,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Optional;
 
+import static javax.management.Query.eq;
+import static javax.swing.UIManager.put;
+import static org.hamcrest.Matchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -62,5 +69,31 @@ public class VetControllerTest {
 
         Optional<Vet> deletedVet = vetRepository.findById(savedVet.getId());
         assertFalse(deletedVet.isPresent(), "El Vet debería haber sido eliminado");
+    }
+
+    @Test
+    public void testUpdateVet() throws Exception {
+        // Crear un veterinario existente
+        Vet vet = new Vet();
+        vet.setNombre("John");
+        vet.setApellido("Doe");
+        vet.setNumeroLicencia("12345");
+        vet.setEspecialidad("Cardiología");
+
+        // Guardar el veterinario en la base de datos
+        Vet savedVet = vetRepository.save(vet);
+
+        // JSON con la información actualizada
+        String updatedVetJson = "{\"nombre\":\"Jane\",\"apellido\":\"Smith\",\"numeroLicencia\":\"67890\",\"especialidad\":\"Dermatología\"}";
+
+        // Realizar la solicitud de actualización
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/vets/{id}", savedVet.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updatedVetJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nombre", is("Jane")))
+                .andExpect(jsonPath("$.apellido", is("Smith")))
+                .andExpect(jsonPath("$.numeroLicencia", is("67890")))
+                .andExpect(jsonPath("$.especialidad", is("Dermatología")));
     }
 }
